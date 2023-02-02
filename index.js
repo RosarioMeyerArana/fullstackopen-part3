eaa5913bf35d4fcb641b78d0220985e3667e1c69
rosario meyer arana <rosario.meyera@gmail.com>
2023-02-02 17:17:33 +0100

    3.16

--- index.js
+++ index.js
@@ -13,28 +13,19 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
-let persons = [
-        { 
-          name: "Arto Hellas", 
-          number: "040-123456",
-          id: 1
-        },
-        { 
-          name: "Ada Lovelace", 
-          number: "39-44-5323523",
-          id: 2
-        },
-        { 
-          name: "Dan Abramov", 
-          number: "12-43-234345",
-          id: 3
-        },
-        { 
-          name: "Mary Poppendieck", 
-          number: "39-23-6423122",
-          id: 4
-        }
-]
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -47,14 +38,15 @@ app.get('/info', (request, response) => {
     </div>`)
 })
 
+
 app.get('/api/persons', (request, response) => {
   Person.find({}).then((res) => {
     response.json(res)
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
 
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
   .then((pers) => {
     if(pers) {
@@ -65,19 +57,17 @@ app.get('/api/persons/:id', (request, response) => {
       })
     }
   })
-  .catch((err) => {
-    response.status(400).send({ error: 'malformatted id' })
-  })
+  .catch((err) => next(err))
 
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
     .then((res) => response.status(204).end())
-    .catch((err) =>  response.status(400).send({ 
-      error: 'malformatted id' 
-    }))
+    .catch((error) => next(error))
 })
+
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -101,11 +91,9 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
-  app.use(unknownEndpoint)
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
